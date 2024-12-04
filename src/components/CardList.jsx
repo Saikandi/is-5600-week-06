ad33dad3ae69db7796da9d92afb5d4b9dac09f53
Saikandi <sai.kandi@slu.edu>
2024-12-04 03:02:42 +0000

    week-06-lab

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,20 @@
-const CardList = () => {
-  return ();
+App.jsx
+import Header from './components/Header';
+import CardList from './components/CardList';
+import { Route, Routes} from 'react-router-dom';
+import productData from './data/full-products';
+import SingleView from './components/SingleView';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+    </div>
+  );
 }
 
-export default CardList;
+export default App;
